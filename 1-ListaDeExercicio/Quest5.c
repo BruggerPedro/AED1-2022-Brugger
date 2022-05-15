6213f4ccbc8206b9ce93a6b440ec7118bc31ad5d
Pedro Brügger <65832126+BruggerPedro@users.noreply.github.com>
2022-05-15 13:53:33 -0300

    Update Quest5.c

--- 1-ListaDeExercicio/Quest5.c
+++ 1-ListaDeExercicio/Quest5.c
@@ -1,1 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+int main()
+{
+    int linhas, i, j, num = 1;
+    
+    printf("------ TRIANGULO DE FLOYD ------\n\n");
+    printf("Insira o numero de linhas: ");
+    scanf("%d",&linhas);
+    printf("\n\n------ RESULTADO ------\n\n");
+    for(i=1; i <= linhas; i++)
+    {
+        for(j=1; j <= i; ++j)
+        {
+            printf("%d ", num);
+            ++num;
+        }
+
+        printf("\n");
+    }
+
+    return 0;
+}
