5314614f21b74125db380fcc8d9cb07d81c34dd5
Pedro Brügger <65832126+BruggerPedro@users.noreply.github.com>
2022-05-15 17:04:17 -0300

    Update Quest6.c

--- 1-ListaDeExercicio/Quest6.c
+++ 1-ListaDeExercicio/Quest6.c
@@ -1,1 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+int fat(int num){
+    if(num == 0){
+        return 1;
+    }
+    if(num == 1){
+        return num;
+    }
+    
+    num = num * fat(num - 1);
+}
+
+
+int main(){
+    int i, ini = 1, fim= 10;
+    
+    printf("-------- FATORIAL --------\n\n");
+    for(i = ini; i <= fim; i++){
+        printf("%d! = %d\n", i, fat(i));
+    }
+    return 0;
+}
