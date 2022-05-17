d28be13e868e817452f9ebefb5af37725f0eba29
Pedro Brügger <65832126+BruggerPedro@users.noreply.github.com>
2022-05-17 15:19:33 -0300

    Update Quest9.c

--- 1-ListaDeExercicio/Quest9.c
+++ 1-ListaDeExercicio/Quest9.c
@@ -1,1 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+int main(){
+    
+    char string[100];
+    int i = 0;
+    
+    printf("---------- STRING MAIUSCULA ----------\n\n");
+    printf("Informe a string desejada: "); 
+    scanf("%[^\n]", string);
+
+    while( string[i] != '\0' ) 
+    {
+       if( string[i] >= 'a' && string[i] <= 'z' )
+          string[i] = string[i] - 32;
+       i++;
+    }
+
+    printf("\nA saida eh: "); 
+    puts(string);
+
+    return 0;
+}
