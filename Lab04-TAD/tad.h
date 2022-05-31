848c226cc69e67445ecbba83e172ec58e22c45f0
Pedro Brügger <65832126+BruggerPedro@users.noreply.github.com>
2022-05-31 17:33:39 -0300

    Update tad.h

--- Lab04-TAD/tad.h
+++ Lab04-TAD/tad.h
@@ -1,1 +1,7 @@
+typedef struct ponto Ponto;
 
+Ponto * gera_pto()
+int set_pto(int x, int y, Ponto *pto);
+int get_pto(Ponto *pto, int *x, int *y)
+float dist_pto(Ponto *p1, *p2)
+void libera_pto(Ponto **pto)
